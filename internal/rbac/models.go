6ff4bf82d5d905e7293823930b76442009fa65b0
Julian Stephens <julian@julianstephens.net>
2025-09-22 12:08:21 -0400

    feat: add seeder module

--- internal/rbac/models.go
+++ internal/rbac/models.go
@@ -1,15 +1,51 @@
 package rbac
 
+import "time"
+
+type RbacRoleDto struct {
+	RbacRole
+	Permissions []RbacPermission `json:"permissions"`
+}
+
 type RbacRole struct {
-	ID          string   `json:"id"`
-	Name        string   `json:"name"`
-	Permissions []string `json:"permissions"`
-	CreatedAt   string   `json:"created_at"`
+	ID           int    `json:"-"`
+	PublicRoleID string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type CreateRbacRoleRequest struct {
+	PublicRoleID string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
 }
 
 type RbacUser struct {
 	ID        string `json:"id"`
 	Email     string `json:"email"`
 	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CreateRbacUserRequest struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+type RbacPermission struct {
+	ID          int    `json:"-"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type CreateRbacPermissionRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type RbacRolePermission struct {
+	RoleName       string `json:"role_name"`
+	PermissionName string `json:"permission_name"`
 }
