f418a5142ecaaf4627046e81baa594349e7ef2bb
Julian Stephens <julian@julianstephens.net>
2025-09-27 13:16:08 -0400

    fix: remove default jwt secret

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,7 +13,7 @@ type Config struct {
 	PostgresURL       string `envconfig:"POSTGRES_URL"`
 	FlagServicePrefix string `envconfig:"FLAG_SERVICE_PREFIX" default:"/featureflags/"`
 	APIVersion        string `envconfig:"API_VERSION" default:"v1"`
-	JWTSecret         string `envconfig:"JWT_SECRET" default:"supersecret"`
+	JWTSecret         string `envconfig:"JWT_SECRET"`
 	JWTExpiry         int    `envconfig:"JWT_EXPIRY" default:"3600"`
 	JWTIssuer         string `envconfig:"JWT_ISSUER" default:"feature-flag-service"`
 }
